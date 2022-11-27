e0c62d32031414bac3a1c5e3979dd1d48e672860
Alejandro Wadel <alejorwad@gmail.com>
2022-11-26 21:25:25 -0300

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\VehiculoController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('vehiculos', VehiculoController::class);
